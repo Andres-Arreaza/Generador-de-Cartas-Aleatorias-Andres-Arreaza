82a3730e4188c5680ffc752c124f1dc581be443c
Andrés Arreaza <112995166+Andres-Arreaza@users.noreply.github.com>
2024-09-10 05:42:40 +0000

    Entrega 1 del generador de cartas-AA

--- src/app.js
+++ src/app.js
@@ -5,7 +5,38 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+// Creación de los arreglos.
+const valor = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const iconos = ["♦", "♥", "♠", "♣"];
+
+// Función para generar un valor random.
+const generadorValorRandom = () => {
+  let valorRandom = Math.floor(Math.random() * valor.length);
+  return valor[valorRandom];
+};
+
+// Función para generar un icono random.
+const generadorIconoRandom = () => {
+  let iconosRandom = Math.floor(Math.random() * iconos.length);
+  return iconosRandom;
+};
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let valorCard = generadorValorRandom();
+  let iconoCardIndex = generadorIconoRandom();
+  let iconoCard = iconos[iconoCardIndex];
+
+  let iconoSuperiorFinal = document.querySelector("#iconoSuperior");
+  let iconoInferiorFinal = document.querySelector("#iconoInferior");
+  let valorFinal = document.querySelector("#valor");
+
+  iconoSuperiorFinal.innerHTML = iconoCard;
+  iconoInferiorFinal.innerHTML = iconoCard;
+  valorFinal.innerHTML = valorCard;
+
+  // Restriccion para que si el index del icono es 0 o 1 me agrege una nueva clase.
+  if (iconoCardIndex === 0 || iconoCardIndex === 1) {
+    iconoSuperiorFinal.classList.add("red");
+    iconoInferiorFinal.classList.add("red");
+  }
 };
